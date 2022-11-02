7fba99e8112b8747cac455620e65121baf656a7d
acidlemon <1667640576@qq.com>
2022-11-03 01:33:48 +0800

    cf1015f

--- cf/1015/f.cpp
+++ cf/1015/f.cpp
@@ -19,14 +19,15 @@ const int mod = 7 + 1e9;
 // const int mod = 998244353;
 const int N = 3 + 233;
 
-LL dp[N][N][2];
+LL dp[N][N][N];
 int n, m;
 char s[N];
-int nxt[N];
+int nxt[N], p[N][2];
 
 int main() {
     cin >> n >> s + 1;
     m = strlen(s + 1);
+
     for (int i = 2, j = 0; i <= m; ++i) {
         while (j && s[j + 1] != s[i]) {
             j = nxt[j];
@@ -36,11 +37,52 @@ int main() {
         }
         nxt[i] = j;
     }
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            
+
+    for (int i = 1; i <= m; ++i) {
+        int j = i;
+        while (j && s[j + 1] != '(') {
+            j = nxt[j];
         }
+        if (s[j + 1] == '(') {
+            ++j;
+        }
+        p[i][0] = j;
+
+        j = i;
+        while (j && s[j + 1] != ')') {
+            j = nxt[j];
+        }
+        if (s[j + 1] == ')') {
+            ++j;
+        }
+        p[i][1] = j;
     }
 
+    ***dp = 1;
+    for (int i = 0; i < 2 * n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            for (int k = 0; k < m; ++k) {
+                dp[i][j][k] %= mod;
+                if (s[k + 1] == '(') {
+                    dp[i + 1][j + 1][k + 1] += dp[i][j][k];
+                } else {
+                    dp[i + 1][j + 1][p[k][0]] += dp[i][j][k];
+                }
+                if (j >= 1) {
+                    if (s[k + 1] == ')') {
+                        dp[i + 1][j - 1][k + 1] += dp[i][j][k];
+                    } else {
+                        dp[i + 1][j - 1][p[k][1]] += dp[i][j][k];
+                    }
+                }
+            }
+            dp[i][j][m] %= mod;
+            dp[i + 1][j + 1][m] += dp[i][j][m];
+            if (j >= 1) {
+                dp[i + 1][j - 1][m] += dp[i][j][m];
+            }
+        }
+    }
+    cout << dp[2 * n][0][m] % mod << endl;
     return 0;
 }
